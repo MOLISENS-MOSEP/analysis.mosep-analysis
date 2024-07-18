b0c743862cfdc3da8a6614675ab339fcbce2f8f4
Christoph Gaisberger <chrisi@all-crew.com>
2024-07-18 15:56:45 +0000

    Add another bag file for analysis (nr. 17) and work on model NB

--- notebooks/papermill/execute_notebooks.py
+++ notebooks/papermill/execute_notebooks.py
@@ -3,14 +3,18 @@ import papermill as pm
 from src.data import config
 
 parameters = {
-    "01": {
-        "BAG_NAME": "molisens_met_2023_08_29-06_04_46_converted",
-        "DATA_DIR": config.INTERIM_DATA_FOLDER / "ViF_Roof" / "data",
+    "0": {
+        "BAG_NAME": "molisens_met_2023_08_07-15_36_45_converted",
+        "DATA_DIR": str(config.INTERIM_DATA_FOLDER / "ViF_Roof" / "data"),
     },
-    # "02": {
-    #     "BAG_NAME": "molisens_met_2023_08_29-06_04_46_converted",
-    #     "DATA_DIR": config.INTERIM_DATA_FOLDER / "ViF_Roof" / "data",
-    #    },
+    "12": {
+        "BAG_NAME": "molisens_met_2023_08_29-06_04_46_converted",
+        "DATA_DIR": str(config.INTERIM_DATA_FOLDER / "ViF_Roof" / "data"),
+    },
+    "17": {
+        "BAG_NAME": "molisens_met_2023_08_29-08_21_47_converted",
+        "DATA_DIR": str(config.INTERIM_DATA_FOLDER / "ViF_Roof" / "data"),
+    },
 }
 
 for run, params in parameters.items():
